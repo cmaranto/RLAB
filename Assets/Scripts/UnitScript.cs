8831f66c8cb8b0c6d0cb60ce68aae3249f51a32d
CJ Maranto <cmaranto07@gmail.com>
2022-02-08 12:06:10 -0500

    ii

---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    public UnitScriptableObject unit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(unit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UnitScriptableObject.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Units",menuName = "ScriptableObjects/UnitScriptableObject",order =1)]
+public class UnitScriptableObject : ScriptableObject
+{
+    public enum ArmorType{
+        Light,
+        Medium,
+        Heavy
+    }
+
+    public enum DamageType{
+        Blunt,
+        Magic,
+        Piercing
+    }
+
+    public enum Race{
+        Human,
+        Orc,
+        Elf,
+        Halfling
+    }
+
+    public enum Class{
+        Warrior,
+        Mage,
+        Archer,
+        Thief
+    }
+
+    public enum Trait{
+        Hearty,
+        Swift,
+        Strong,
+        Studious
+    }
+
+    public string unitName = "Unit";
+    public int range = 0;
+    public int baseVitality = 1;
+    public int baseSpeed = 1;
+    public int baseStrength = 1;
+    public int baseIntelligence = 1;
+
+    public Race race;
+    public Class unitClass;
+    public Trait[] traits;
+
+    public override string ToString(){
+        return string.Format("{0}: {1} {2}",name,race,unitClass);
+    }
+
+    
+
+}
